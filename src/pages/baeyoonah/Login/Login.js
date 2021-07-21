d2901c5309266143ca0462b512352a1cb1b932b2
user1 <user1@user1ui-MacBook-Air.local>
2021-07-21 21:30:20 +0900

    Add : 로그인 컴포넌트 및 로그인 CSS 추가  # Please enter the commit message for your changes. Lines starting

--- src/pages/baeyoonah/Login/Login.js
+++ src/pages/baeyoonah/Login/Login.js
@@ -1,13 +1,43 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import './Login.scss';
 
 class LoginAh extends React.Component {
+  goToMain = () => {
+    this.props.history.push('/MainAh');
+  };
+
   render() {
     return (
-      <div>
-        <p>다슬</p>
+      <div className="Login">
+        <div className="container">
+          <div className="loginContainer">
+            <p>westagram</p>
+            <div className="idContainer">
+              <input
+                type="text"
+                id="inputId"
+                placeholder="전화번호, 사용자 이름 또는 이메일"
+              />
+            </div>
+            <div className="passwordContainer">
+              <input
+                type="password"
+                id="inputPassword"
+                placeholder="비밀번호"
+              />
+            </div>
+            <button className="loginButton" onClick={this.goToMain}>
+              로그인
+            </button>
+            <div>
+              <a href="#">비밀번호를 잊으셨나요?</a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default LoginAh;
+export default withRouter(LoginAh);
