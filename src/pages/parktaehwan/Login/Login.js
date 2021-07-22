fcedada589847d4033304617c510ea290979411f
mekemeke94 <thth3194@gmail.com>
2021-07-22 13:18:05 +0900

    ADD: 로그인 페이지 및 scss 컴포넌트 추가

--- src/pages/parktaehwan/Login/Login.js
+++ src/pages/parktaehwan/Login/Login.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import './Login.scss';
 
 class LoginHwan extends React.Component {
   render() {
     return (
-      <div>
-        <p>명성</p>
-      </div>
+      <body className="loginBody">
+        <div className="Loginmain">
+          <div className="mainWrap">
+            <header className="logo">westagram</header>
+            <section className="loginInputs">
+              <div className="idPw">
+                <input
+                  id="inputId"
+                  className="inputId"
+                  type="text"
+                  placeholder="전화번호, 사용자 이름 또는 이메일"
+                />
+                <input
+                  id="inputPw"
+                  className="inputPw"
+                  type="password"
+                  placeholder="비밀번호"
+                />
+              </div>
+              <div className="btn">
+                <button onclick={this.goToMain} className="loginBtn">
+                  로그인
+                </button>
+              </div>
+            </section>
+            <footer>
+              <div className="forgotPw">
+                <a className="forgotPwLink" href="#">
+                  비밀번호를 잊으셨나요?
+                </a>
+              </div>
+            </footer>
+          </div>
+        </div>
+      </body>
     );
   }
 }
